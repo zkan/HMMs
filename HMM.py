50a5e234d3a4969b500fb4cd47574bbaf901977e
Kan O <zkan.cs@gmail.com>
2012-03-06 11:23:13 +0700

    add a simple and stupid HMM class

---
+++ HMM.py
@@ -0,0 +1,23 @@
+class HMM:
+    '''A HMM class'''
+    _pi = 0.0
+    _A = 0.0
+    _B = 0.0
+
+    def __init__(self):
+        self._pi = 5.0
+        self._A = 0.0
+        self._B = 0.0
+
+    def get_pi(self):
+        return self._pi
+
+    def get_A(self):
+        return self._A
+
+    def get_B(self):
+        return self._B
+    
+    def display(self):
+        print 'pi = ', self._pi
+        print 'A = ', self._A
---
+++ main.py
@@ -0,0 +1,4 @@
+from HMM import HMM
+
+hmm = HMM()
+hmm.display()
